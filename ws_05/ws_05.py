d74936bb85e8067fe814d3edf46292a983527700
RA <permale.rey@gmail.com>
2024-04-24 23:25:17 +1000

    quiz solutions

---
+++ ws_05/ws_05.py
@@ -0,0 +1,19 @@
+#1
+#
+# string_array = []
+# longest = 0
+# longest_string = ''
+#
+# while True:
+#
+#     string = str(input("Enter a string:"))
+#     string_array.append(string)
+#     if string.startswith("A"):
+#         break
+#
+#     if len(string) > longest:
+#         longest = len(string)
+#         longest_string = string
+#
+# print("The longest was:", longest_string)
+
